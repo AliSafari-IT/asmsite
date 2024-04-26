e21cde657aa267dad1f20b69ba6c9c8125711bd1
asafarim <asafarim.it@gmail.com>
2024-04-26 23:57:25 +0200

    client-docs app added to nx workspace

--- client-react/src/app/app.tsx
+++ client-react/src/app/app.tsx
@@ -11,7 +11,7 @@ export function App() {
   return (
     <div>
       <NxWelcome title="client-react" />
-
+      
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
       {/* Feel free to move and update them to fit your needs */}
